d6d45bb2d0c45438ca85005d48fa1e38b5313681
GaneshBEmech <gbhosale@google.com>
2026-03-25 10:49:08 +0000

    updated main.tf

--- TASK1/main.tf
+++ TASK1/main.tf
@@ -27,4 +27,27 @@ resource "google_compute_instance" "task1-instance-1" {
  
 }
 
+resource "google_compute_instance" "task1-instance-2" {
+  name = "task1-instance-2"
+  machine_type = "e2-micro"
+  zone = "us-east1-b"
+  boot_disk {
+    initialize_params { 
+      image = "debian-11-bullseye-v20260310"
+    }
+  }
+  
+  network_interface {
+    network = google_compute_network.custom_network.id
+    subnetwork = google_compute_subnetwork.us-east1.id 
+  }
+  
+   depends_on = [
+     google_compute_network.custom_network,
+     google_compute_subnetwork.us-central1,
+     google_compute_subnetwork.us-east1
+   ]
+ 
+}
+
 
